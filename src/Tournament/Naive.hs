92884bb851fdaf5af973b0435a59324aae14ed89
Infinite Lifes <ataauri@gmail.com>
2018-05-18 15:00:36 +0200

    Removed tail from unary constructor for 'Tournament.Naive.Alternative', since the expected behaviour is to always have pairs in front and optional single at the end, ergo. there is no non-intermediate state where single element can be found at the begining of the bracket

--- src/Tournament/Naive.hs
+++ src/Tournament/Naive.hs
@@ -1,62 +1,66 @@
-module Tournament.Naive(chooseEntry) where
+module Tournament.Naive(chooseEntryIO) where
 
 import Core.Data
 import Core.Bracket
-import Core.Parser
 
 import Data.Monoid
+import Control.Monad (ap)
 
-import qualified Control.Applicative as Ap
-
-data Alternative a = Two a a (Alternative a) | One a (Alternative a) | Null
+data Alternative a = Two a a (Alternative a) | One a | Null
 
 instance Bracket Alternative where
-    reduce (Two x y xs) (All True)  = xs `join` (One x Null)
-    reduce (Two x y xs) (All False) = xs `join` (One y Null)
-    reduce (One x xs)    _          = xs `join` (One x Null)
+    reduce (Two x y xs) (All True)  = mappend xs (One x)
+    reduce (Two x y xs) (All False) = mappend xs (One y)
+    reduce (One x)       _          = One x
     reduce _             _          = Null
     
     getChoices (Two x y _) = [x,y]
-    getChoices (One x _)   = [x]
+    getChoices (One x)     = [x]
     getChoices _           = []
+
+instance Semigroup (Alternative a) where
+    (<>) x            Null         = x
+    (<>) Null         x            = x
+    (<>) (One x)      (One y)      = Two x y Null
+    (<>) (One x)      (Two y z xs) = Two x y (One z <> xs)
+    (<>) (Two x y xs) x'           = Two x y (xs <> x')
+
+instance Monoid (Alternative a) where
+    mempty = Null
 
 instance Functor Alternative where
     fmap _  Null        = Null
-    fmap f (One x xs)   = (One (f x) (fmap f xs))
+    fmap f (One x)      = (One (f x))
     fmap f (Two x y xs) = (Two (f x) (f y) (fmap f xs))
-  
-chooseEntry :: Monoid a => ([ a ] -> IO Choice) -> [ a ] -> IO a
-chooseEntry f x = (reduceEntries f) $ makeAlternatives x
 
-join :: Alternative a -> Alternative a -> Alternative a
-join  Null        x  = x
-join (One x Null) x' = collapse (One x x')
-join (One x xs)   x' = join (collapse (One x xs)) x'
-join (Two x y xs) x' = Two x y (join xs x')
+instance Applicative Alternative where
+    pure x = One x
+    (<*>) = ap
+     
+instance Monad Alternative where
+    (>>=) Null         _  = Null
+    (>>=) (One x)      f = f x
+    (>>=) (Two x y xs) f = f x `mappend` f y `mappend` (xs >>= f)
 
+chooseEntryIO :: Monoid a => ([ a ] -> IO Choice) -> [ a ] -> IO a
+chooseEntryIO f x = (reduceEntriesIO f) $ makeAlternatives x
 
-collapse :: Alternative a -> Alternative a
-collapse  Null                  = Null
-collapse (One x    Null       ) = One x Null
-collapse (One x   (One y xs)  ) = Two x y (collapse xs)
-collapse (One x   (Two y z xs)) = Two x y (collapse (One z xs))
-collapse (Two x y  xs         ) = Two x y (collapse xs)
 
 
 altMap :: (Alternative a -> Alternative b) -> Alternative a -> Alternative b
-altMap _  Null        = Null
-altMap f (One x   xs) = join (f (One x   Null)) (altMap f xs)
-altMap f (Two x y xs) = join (f (Two x y Null)) (altMap f xs)
+altMap _ Null         = Null
+altMap f (One x)      = (f (One x))
+altMap f (Two x y xs) = (f (Two x y Null)) `mappend` (altMap f xs)
 
+reduceEntries :: Monoid a => Choice -> Alternative a -> Alternative a
+reduceEntries = flip reduce
 
-reduceEntries :: Monoid a => ([ a ] -> IO Choice) -> Alternative a -> IO a
-reduceEntries _ Null = return mempty
-reduceEntries _ (One x Null) = return x
-reduceEntries f x = f (getChoices x) >>= return . (reduce x) >>= reduceEntries f
+reduceEntriesIO :: Monoid a => ([ a ] -> IO Choice) -> Alternative a -> IO a
+reduceEntriesIO _ Null    = return mempty
+reduceEntriesIO _ (One x) = return x
+reduceEntriesIO f x       = f (getChoices x) >>= return . (reduce x) >>= reduceEntriesIO f
 
 makeAlternatives :: [ a ] -> Alternative a
 makeAlternatives [] = Null
-makeAlternatives [x] = One x Null
+makeAlternatives [x] = One x
 makeAlternatives (x:y:xs) = Two x y (makeAlternatives xs)
-
-
