d2de8348b8c4fb14d93c8ea47ed932ad689b554a
Infinite Lifes <ataauri@gmail.com>
2018-06-08 14:32:27 +0200

    Integrated Cli and Webapp code

--- server/haskell/src/Main.hs
+++ server/haskell/src/Main.hs
@@ -3,5 +3,8 @@ module Main where
 import Server.Server
 import Core.Cli
 
+import System.Environment
+import Control.Arrow
+
 main :: IO ()
-main = mainServer
+main = getArgs >>= (any (== "-cli") >>> (\b -> if b then mainCLI else mainServer))
